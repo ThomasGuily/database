d6a23424bfca3a6fd54addbc54299f708caeec55
mathisdelehouzee <mathdel@outlook.com>
2018-12-29 19:41:28 +0100

    Changemet du header et indexe

--- mathis/index.php
+++ mathis/index.php
@@ -1,38 +1,6 @@
-﻿<?php require 'header.php'; ?>
+<?php require 'header.php'; ?>
 
-<body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">BioCycle</a> <!--  Ajouter le nom dans le containeur-->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.html">
-                            Accueil
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="notreentreprise.html">Notre entreprise</a>
-                    </li>                 
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.html">Connexion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Mon panier</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
     <!-- Page Content -->
     <div class="container">
