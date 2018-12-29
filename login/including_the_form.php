20748cf1fab2d992661d566f259c620ce3d00ce2
gmagannaDevelop <gmaganna.biomed@gmail.com>
2018-12-29 15:24:03 +0000

    Still writing the login form. It is not ready yet.

--- login/including_the_form.php
+++ login/including_the_form.php
@@ -1,10 +1,28 @@
 
 # Pour faire marcher le login il faut au moins avoir une <form> tag comme on peut voir en bas. En plus le button doit avoir pour nom (name="signup-submit").
 
+# La peut-etre il faudra verifier ce que le gars a mis dapns header.php et footer.php
+
+<?php
+   require "header.php";
+?>
 
 <main>
-   <form class="form-signup" action="login/signup.inc.php" method="post">
-      <button type="submit" name="signup-submit"> Signup </button>
-      
-   </form>
+   <div class="wrapper-main">
+      <section class="section-default">
+         <h1>Signup</h1>
+            <form class="form-signup" action="login/signup.inc.php" method="post">
+               <input type="text" name="uid"        placeholder="Username">
+               <input type="text" name="mail"       placeholder="E-mail">
+               <input type="password" name="pwd"        placeholder="Password">
+               <input type="password" name="pwd-repeat" placeholder="Repeat password">
+               <button type="submit" name="signup-submit"> Signup </button>
+            </form>
+      </section>
+   </div>
 </main>
+
+<?php
+   require "footer.php";
+?>
+
--- login/signup.inc.php
+++ login/signup.inc.php
@@ -3,6 +3,14 @@
 if (isset($_POST['signup-submit'])) {
    require 'dbh.inc.php';
    $username = $_POST['uid'];
+   $email    = $_POST['mail'];
+   $password = $_POST['pwd'];
+   $passwrep = $_POST['pwd-repeat'];
+
+   if (empty($username) || empty($email) || empty($password) || empty($passwrep)) {
+      header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail".$email);
+      exit();
+   }
 }
 
 ?>
