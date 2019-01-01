2b47a88520bce49d635dd187047cfdd4a462e214
mathisdelehouzee <mathdel@outlook.com>
2019-01-01 12:30:28 +0100

    Debug footer notreentreprise.php

--- mathis/notreentreprise.php
+++ mathis/notreentreprise.php
@@ -86,16 +86,6 @@
     </section>
 
 
-    <!-- Footer -->
-    <footer class="py-5 bg-black">
-        <div class="container">
-            <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2018</p>
-        </div>
-        <!-- /.container -->
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+   
 
  <?php require 'footer.php'; ?>
