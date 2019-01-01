004a83f2e4994efe1dc024ce03866b7e9d529c27
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-01-01 19:55:14 +0100

    BioCycle button now redirects to index.php

--- mathis/header.php
+++ mathis/header.php
@@ -55,7 +55,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#">BioCycle</a> <!--  Ajouter le nom dans le containeur-->
+            <a class="navbar-brand" href="index.php">BioCycle</a> <!--  Ajouter le nom dans le containeur-->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
