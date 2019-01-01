ebedc9304a05b4cb3f9cc9353dea20182f9029eb
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-01-01 19:04:46 +0100

    pagevente.php now uses the standard header.php

--- mathis/pagevente.php
+++ mathis/pagevente.php
@@ -1,30 +1,10 @@
 ﻿
-<!DOCTYPE html>
-<html lang="en">
-   
+<?php
+    require "header.php";
+?>
 
-<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Shop Homepage - Start Bootstrap Template</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/shop-homepage.css" rel="stylesheet">
-    <link href="css/4-col-portfolio.css" rel="stylesheet">
-    <?php
-      include_once 'includes/dbh.inc.php';
-      // la variable qui contient la connexion:
-      // $connexion
-
-      include_once 'includes/dbh.class.php';
-      $DB = new DB();
+<?php
 
       $option    = $_GET['button'];
       $my_origin = $_GET['origin'];
@@ -62,46 +42,9 @@
         default:
           die("$option seems to be missing");
       }
-    ?>
-</head>
+?>
 
-<body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">BioCycle</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">
-                            Accueil
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="notreentreprise.html">Notre entreprise</a>
-                    </li>
-                    
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php">Connexion</a>
-                    </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#">Mon panier</a>
-                    </li>
-                    -->
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-        <!-- Page Content -->
+<!-- Page Content -->
         <div class="container">
             <!-- Page Heading -->
             <h1 class="my-4">
